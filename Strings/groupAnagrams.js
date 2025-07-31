3cd038d6b4af2b76a96328323e339b726000b3bd
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2025-07-31 11:25:47 -0400

    String : Group Anagrams

---
+++ Strings/groupAnagrams.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  // Approach 1 - Sorting and storing inside map
+  const store = new Map();
+
+  for (let i = 0; i < strs.length; i++) {
+    let str = strs[i].split("").sort().join("");
+    if (!store.has(str)) {
+      store.set(str, [strs[i]]);
+    } else {
+      store.get(str).push(strs[i]);
+    }
+  }
+
+  return [...store.values()];
+};
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams2 = function (strs) {
+  const store = new Map();
+
+  for (let i = 0; i < strs.length; i++) {
+    let freqArr = Array(26).fill(0);
+
+    let s = strs[i];
+    for (let j = 0; j < s.length; j++) {
+      let index = s[j].charCodeAt() - "a".charCodeAt();
+      ++freqArr[index];
+    }
+
+    let key = "";
+    for (let k = 0; k < 26; k++) {
+      key = key + String.fromCharCode(k) + freqArr[k];
+    }
+
+    if (!store.has(key)) {
+      store.set(key, [strs[i]]);
+    } else {
+      store.get(key).push(strs[i]);
+    }
+  }
+
+  return [...store.values()];
+};
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+console.log(groupAnagrams2(["eat", "tea", "tan", "ate", "nat", "bat"]));
