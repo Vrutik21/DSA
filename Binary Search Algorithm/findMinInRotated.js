bc6c193f4d2729daa06196c19ff92c14d31cb2d4
Vrutik Parmar <parmarvrutik1@gmail.com>
2025-08-10 11:48:43 -0400

    BSA : Find Minimum in sorted array

---
+++ Binary Search Algorithm/findMinInRotated.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return nums[left];
+};
+
+console.log(findMin([5, 1, 2, 3, 4]));
