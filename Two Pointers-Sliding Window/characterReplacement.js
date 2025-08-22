afe0dce5de60da86ce16d0a272593b7394757669
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2025-08-22 16:31:31 -0400

    Sliding Window : Longest Repeating Character Replacement

--- Two Pointers-Sliding Window/characterReplacement.js
+++ Two Pointers-Sliding Window/characterReplacement.js
@@ -1,38 +1,83 @@
+// 424
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement1 = function (s, k) {
+  // My version
+  // TC - O(n)
+  // SC - O(1)
+  let store = new Map();
+  let maxCount = 0;
+  let i = 0;
+  let j = 0;
+  store.set(s[0], 1);
+
+  while (j < s.length) {
+    let maxStoreCount = 0;
+    let totalStoreCount = 0;
+
+    store.forEach((val, key) => {
+      maxStoreCount = Math.max(maxStoreCount, val);
+      totalStoreCount += val;
+    });
+
+    if (totalStoreCount - maxStoreCount <= k) {
+      maxCount = Math.max(maxCount, totalStoreCount);
+      ++j;
+      // store[s[j]] = store[s[j]] ? ++store[s[j]] : 1;
+      store.set(s[j], store.get(s[j]) ? store.get(s[j]) + 1 : 1);
+    } else {
+      store.set(s[i], store.get(s[i]) - 1);
+      // --store[s[i]];
+      ++i;
+    }
+  }
+
+  return maxCount;
+};
+
 /**
  * @param {string} s
  * @param {number} k
  * @return {number}
  */
 var characterReplacement = function (s, k) {
-  let i = (j = 0);
-  let longest = 0;
-  let newString = "";
-  let kCounter = k;
-
-  if (k <= 0) {
-    return 1;
-  }
+  let store = {};
+  let maxCount = 0;
+  let i = 0;
+  let j = 0;
+  store[s[0]] = 1;
 
   while (j < s.length) {
-    console.log("newString", newString);
-    if (s[i] !== s[j]) {
-      if (kCounter > 0) {
-        newString += s[i];
-        --kCounter;
-      } else {
-        kCounter = k;
-        ++i;
-        j = i + 1;
-        newString = "";
-      }
-    } else {
-      newString += s[j];
+    if (isWindowValid(store, k)) {
+      maxCount = Math.max(maxCount, j - i + 1);
       ++j;
+      store[s[j]] = store[s[j]] ? ++store[s[j]] : 1;
+    } else {
+      --store[s[i]];
+      ++i;
     }
-    longest = Math.max(newString.length, longest);
   }
 
-  return longest;
+  return maxCount;
+};
+
+var isWindowValid = (store, k) => {
+  let totalCount = 0;
+  let maxCount = 0;
+
+  for (let i = 0; i < 26; i++) {
+    // 65 = A, 66 = B
+    let char = String.fromCharCode(i + 65);
+    if (store[char]) {
+      totalCount = totalCount + store[char];
+      maxCount = Math.max(maxCount, store[char]);
+    }
+  }
+
+  return totalCount - maxCount <= k;
 };
 
 // console.log(characterReplacement("AABABBA",1))
