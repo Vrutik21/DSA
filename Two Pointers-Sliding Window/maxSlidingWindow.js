37b241b4278cebe1a2ba39d6405baac6c40acf3e
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2025-08-26 11:32:54 -0400

    Sliding Window : Maximum sliding window

--- Two Pointers-Sliding Window/maxSlidingWindow.js
+++ Two Pointers-Sliding Window/maxSlidingWindow.js
@@ -1,3 +1,4 @@
+// 239
 /**
  * @param {number[]} nums
  * @param {number} k
@@ -5,29 +6,27 @@
  */
 var maxSlidingWindow = function (nums, k) {
   let result = [];
-
-  let i = 0;
-  let j = k - 1;
-
-  let max = -Infinity;
-  for (let y = 0; y < j; y++) {
-    if (nums[y] >= max) {
-      max = nums[y];
-    }
-  }
-
-  result.push(max);
-  ++j;
-  ++i;
+  // Using a Monotonic decreasing dequeue
+  let dq = [];
+  let i = (j = 0);
 
   while (j < nums.length) {
-    if (nums[j] >= result[result.length - 1]) {
-      result.push(nums[j]);
-    } else {
-      result.push(result[result.length - 1]);
+    // Push current elements after removing all smaller elements from the last/back
+    while (dq.length && nums[j] > dq[dq.length - 1]) {
+      dq.pop();
+    }
+    dq.push(nums[j]);
+
+    // When window size gets larger than k
+    if (j >= k - 1) {
+      result.push(dq[0]);
+
+      // if previous ith element is larger then remove it from the queue to store the largest for the current window
+      // And then increment the i
+      nums[i] == dq[0] && dq.shift();
+      ++i;
     }
     ++j;
-    ++i;
   }
 
   return result;
