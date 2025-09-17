c1fbda01dd200d62bec1c144153385a95957b35e
Vrutik Parmar <parmarvrutik1@gmail.com>
2025-09-16 22:03:39 -0400

    Trees : Postorder iterative two stack

--- Trees/postorderTraversal.js
+++ Trees/postorderTraversal.js
@@ -1,3 +1,4 @@
+// 145
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
@@ -23,6 +24,38 @@ var postorderTraversal = function (root) {
   }
 
   traversal(root);
+
+  return ans;
+};
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// Iterative method
+// Using two stacks
+var postorderTraversal = function (root) {
+  //   left -> right -> root
+  if (!root) return [];
+
+  let ans = [];
+  // using stack 1 to store the children of the last visited node
+  let stack1 = [root];
+  // using stack 2 to store the last visited nodes
+  // We get the reverse order i.e Root -> right -> left
+  let stack2 = [];
+
+  while (stack1.length) {
+    let curr = stack1.pop();
+    stack2.push(curr);
+
+    if (curr.left) stack1.push(curr.left);
+    if (curr.right) stack1.push(curr.right);
+  }
+
+  while (stack2.length) {
+    ans.push(stack2.pop().val);
+  }
 
   return ans;
 };
