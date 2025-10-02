a1bb1c674cec2517c7550a6ee526d8fd16ba3a19
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2025-10-02 13:24:04 -0400

    Arrays : Best Time to Buy and Sell Stock II

---
+++ Arrays/buySellStockProfit2.js
@@ -0,0 +1,19 @@
+// 122. Best Time to Buy and Sell Stock II
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+// TC - O(n)
+// SC - O(1)
+var maxProfit = function (prices) {
+  let totalProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      totalProfit += prices[i] - prices[i - 1];
+    }
+  }
+
+  return totalProfit;
+};
