4ba40c058e6d513c14c8fd1cbb5d9ee0ded7e06e
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2025-10-31 08:40:29 -0400

    Strings : Longest Palindromic Substring

--- Strings/longestPalindrome.js
+++ Strings/longestPalindrome.js
@@ -6,7 +6,7 @@
  */
 // TC - (n^3)
 // SC - O(n)
-var longestPalindrome = function (s) {
+var longestPalindrome1 = function (s) {
   const n = s.length;
   if (n < 2) return s;
 
@@ -32,4 +32,48 @@ var longestPalindrome = function (s) {
     }
   }
   return best;
+};
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+// TC - (n^2)
+// SC - O(1)
+var longestPalindrome = function (s) {
+  let n = s.length;
+  if (n < 2) return s;
+
+  let bestL = 0;
+  let bestR = 0;
+
+  function expand(s, l, r) {
+    while (l >= 0 && r < s.length && s[l] === s[r]) {
+      l--;
+      r++;
+    }
+
+    // maximal palindrome bounds
+    return [l + 1, r - 1];
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    // For odd length
+    // Ex. racecar
+    let [l1, r1] = expand(s, i, i);
+    if (r1 - l1 > bestR - bestL) {
+      bestL = l1;
+      bestR = r1;
+    }
+
+    // For even length
+    // Ex. abba
+    let [l2, r2] = expand(s, i, i + 1);
+    if (r2 - l2 > bestR - bestL) {
+      bestL = l2;
+      bestR = r2;
+    }
+  }
+
+  return s.slice(bestL, bestR + 1);
 };
