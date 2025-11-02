062cfe1d48d2ab67690d8eb79b5b544c73de1bc6
Vrutik Parmar <parmarvrutik1@gmail.com>
2025-11-02 11:01:51 -0500

    String : String to Integer (atoi)

--- Strings/myAtoi.js
+++ Strings/myAtoi.js
@@ -6,7 +6,7 @@
  */
 // TC - O(n)
 // SC - O(1)
-var myAtoi = function (s) {
+var myAtoi1 = function (s) {
   let temp = "";
   let sign = 1;
   const INT_MIN = -Math.pow(2, 31);
@@ -38,4 +38,50 @@ var myAtoi = function (s) {
   if (finalAns >= INT_MAX) finalAns = INT_MAX;
 
   return finalAns;
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// TC - O(n)
+// SC - O(1)
+var myAtoi = function (s) {
+  const INT_MIN = -Math.pow(2, 31);
+  const INT_MAX = Math.pow(2, 31) - 1;
+
+  let i = 0;
+  const n = s.length;
+
+  // 1) Skip leading spaces
+  while (i < n && s[i] === " ") i++;
+  if (i === n) return 0;
+
+  // 2) sign
+  let sign = 1;
+  if (s[i] === "+" || s[i] === "-") {
+    if (s[i] === "-") sign = -1;
+    i++;
+  }
+
+  // 3) If next is not a digit, return 0
+  if (i >= n || s[i] < "0" || s[i] > "9") return 0;
+
+  // 4) Parse digits until a non-digit
+  let result = 0;
+  while (i < n && s[i] >= "0" && s[i] <= "9") {
+    // 0-9 is represented by numbers 48-57 in Numerical code format
+    const digit = s[i].charCodeAt(0) - "0".charCodeAt(0);
+    result = result * 10 + digit;
+    i++;
+  }
+
+  // 5) Apply sign
+  result *= sign;
+
+  // 6) Adjust to 32-bit signed integer range
+  if (result < INT_MIN) return INT_MIN;
+  if (result > INT_MAX) return INT_MAX;
+
+  return result;
 };
