8ae65d38a174efd877574e31c5ab0d81432eb192
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2025-11-03 16:12:49 -0500

    String : Integer to Roman

---
+++ Strings/intToRoman.js
@@ -0,0 +1,37 @@
+// 12. Integer to Roman
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+// TC - O(1)
+// SC - O(1)
+var intToRoman = function (num) {
+  const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const symbols = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+
+  let res = "";
+
+  for (let i = 0; i < values.length && num > 0; i++) {
+    while (num >= values[i]) {
+      num -= values[i];
+      res += symbols[i];
+    }
+  }
+
+  return res;
+};
