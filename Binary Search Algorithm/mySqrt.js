86143f9a311b211568f0eb569957225847f5ca9d
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2025-11-28 09:53:25 -0500

    Binary Search : Sqrt(x)

--- Binary Search Algorithm/mySqrt.js
+++ Binary Search Algorithm/mySqrt.js
@@ -1,9 +1,10 @@
-// 69
+// 69 Sqrt(x)
+
 /**
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function (x) {
+var mySqrt1 = function (x) {
   if (x < 2) return x;
 
   let left = 2;
@@ -25,4 +26,33 @@ var mySqrt = function (x) {
 
   //   right would be the closest at the end if mid is not the square root.
   return right;
+};
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+// More simplified and easy solution
+// TC - O(log x)
+// SC - O(1)
+var mySqrt = function (x) {
+  if (x < 2) return x;
+
+  let l = 1;
+  let r = x;
+  let res = 0;
+
+  while (l <= r) {
+    let mid = l + Math.floor((r - l) / 2);
+    let sq = mid * mid;
+
+    if (sq <= x) {
+      res = mid;
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+
+  return res;
 };
