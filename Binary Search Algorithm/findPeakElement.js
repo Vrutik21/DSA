b5fc8f11ce8ced842caf4e048d200cfaa99c3a94
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2025-12-03 09:57:11 -0500

    Binary Search : Find Peak Element

--- Binary Search Algorithm/findPeakElement.js
+++ Binary Search Algorithm/findPeakElement.js
@@ -1,8 +1,44 @@
-// 162
+// 162 Find Peak Element
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
+// My solution
+// TC - O(log n)
+// SC - O(1)
+var findPeakElement1 = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (
+      mid !== 0 &&
+      mid !== nums.length - 1 &&
+      nums[mid] > nums[mid - 1] &&
+      nums[mid] > nums[mid + 1]
+    ) {
+      return mid;
+    } else {
+      if (nums[mid + 1] > nums[mid]) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+  }
+
+  return right;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// TC - O(log n)
+// SC - O(1)
 var findPeakElement = function (nums) {
   let left = 0;
   let right = nums.length - 1;
