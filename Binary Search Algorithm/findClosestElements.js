59968e3ec2d2b1e012a66c7b5c260450fe7973f8
Vrutik Parmar <parmarvrutik1@gmail.com>
2025-12-06 18:26:37 -0500

    Binary Search : Find K Closest Elements

--- Binary Search Algorithm/findClosestElements.js
+++ Binary Search Algorithm/findClosestElements.js
@@ -1,9 +1,13 @@
+// 658. Find K Closest Elements
+
 /**
  * @param {number[]} arr
  * @param {number} k
  * @param {number} x
  * @return {number[]}
  */
+// TC - O(n)
+// SC - O(1)
 var findClosestElements1 = function (arr, k, x) {
   // Solving using linear search
   let l = 0;
@@ -28,15 +32,24 @@ var findClosestElements1 = function (arr, k, x) {
  * @param {number} x
  * @return {number[]}
  */
+// TC - O(Log(N−K)) because we reduce the size by k
+//  SC - O(1)
 var findClosestElements = function (arr, k, x) {
   // Solving using binary search
-  //   Tricky to understand but there is a sliding window
+  // Tricky to understand but there is a sliding window
   let l = 0;
-  let r = arr.length - 1;
+  // window size should always be arr.len - k
+  // since we only want k elements from our left pointer
+  // So it does not matter where r is but it should not be out of bounds
+  // JS treats out of bounds as false that's why arr.len - 1 should pass all tests
+  // However, arr.len - k is more practical
+  let r = arr.length - k;
 
   while (l < r) {
     let m = l + Math.floor((r - l) / 2);
 
+    // Because array is sorted → arr[m] ≤ x (usually)
+    // Because arr[m+k] ≥ x
     if (x - arr[m] > arr[m + k] - x) {
       l = m + 1;
     } else {
