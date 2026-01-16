94a011f8f19a401154f25ab36b876082878a61b7
Vrutik Parmar <parmarvrutik1@gmail.com>
2026-01-15 20:41:33 -0500

    Stack : Asteroid Collision

--- Stack/asteroidCollision.js
+++ Stack/asteroidCollision.js
@@ -1,35 +1,35 @@
+// 735. Asteroid Collision
+
 /**
  * @param {number[]} asteroids
  * @return {number[]}
  */
+// TC - O(n)
+// SC - O(n)
 var asteroidCollision = function (asteroids) {
   let stack = [];
 
   for (let i = 0; i < asteroids.length; i++) {
     let curr = asteroids[i];
+    let insert = true;
 
-    while (
-      stack.length > 0 &&
-      ((stack[stack.length - 1] > 0 && curr < 0) ||
-        (curr > 0 && stack[stack.length - 1] < 0))
-    ) {
-      if (Math.abs(stack[stack.length - 1]) < Math.abs(curr)) {
-        stack.pop();
-        stack.push();
-      } else {
-        continue;
+    if (stack.length > 0 && stack[stack.length - 1] > 0 && curr < 0) {
+      while (stack.length > 0 && stack[stack.length - 1] > 0 && curr < 0) {
+        if (Math.abs(stack[stack.length - 1]) < Math.abs(curr)) {
+          stack.pop();
+        } else if (Math.abs(stack[stack.length - 1]) === Math.abs(curr)) {
+          stack.pop();
+          insert = false;
+          break;
+        } else {
+          insert = false;
+          break;
+        }
       }
-    }
-
-    if (
-      stack.length === 0 ||
-      (stack[stack.length - 1] > 0 && curr > 0) ||
-      (stack[stack.length - 1] < 0 && curr < 0)
-    ) {
+      if (insert) stack.push(curr);
+    } else {
       stack.push(curr);
     }
-
-    console.log(stack, "stack");
   }
 
   return stack;
