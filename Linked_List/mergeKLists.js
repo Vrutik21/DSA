61a827f8147032cf1598e22cef7ada142189cb02
Vrutik Parmar <parmarvrutik1@gmail.com>
2026-01-24 17:02:31 -0500

    Linked List : Merge k Sorted Lists

--- Linked_List/mergeKLists.js
+++ Linked_List/mergeKLists.js
@@ -55,3 +55,74 @@ var mergeKLists = function (lists) {
 
   return sentinel.next;
 };
+
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+// Divide & Conquer non-recursive approach
+// TC - O(N * logk)
+// SC - O(1)
+var mergeKLists = function (lists) {
+  if (!lists || lists.length === 0) return null;
+
+  // Interval represents how far apart lists are merged
+  let interval = 1;
+
+  // Keep merging until only one list remains
+  while (interval < lists.length) {
+    for (let i = 0; i + interval < lists.length; i += interval * 2) {
+      lists[i] = mergeTwoLL(lists[i], lists[i + interval]);
+    }
+
+    interval *= 2; // Double the merge range
+  }
+
+  // The fully merged list is stored at index 0
+  return lists[0];
+};
+
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+// Divide & Conquer recursive approach
+// TC - O(N * logk)
+// SC - O(log k) recursion call stack
+var mergeKLists = function (lists) {
+  if (!lists || lists.length === 0) return null;
+
+  return mergeRange(lists, 0, lists.length - 1);
+};
+
+function mergeRange(lists, left, right) {
+  if (left === right) return lists[left];
+
+  const mid = left + Math.floor((right - left) / 2);
+
+  let leftHalfMerged = mergeRange(lists, left, mid);
+  let rightHalfMerged = mergeRange(lists, mid + 1, right);
+
+  return mergeTwoLL(leftHalfMerged, rightHalfMerged);
+}
+
+// Helper function
+function mergeTwoLL(list1, list2) {
+  let sentinel = new ListNode();
+  let prev = sentinel;
+
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      prev.next = list1;
+      list1 = list1.next;
+    } else {
+      prev.next = list2;
+      list2 = list2.next;
+    }
+    prev = prev.next;
+  }
+
+  prev.next = list1 || list2;
+
+  return sentinel.next;
+}
