6bba54a3a639532e998d7b9be31f9f026283fe9a
Vrutik Parmar <parmarvrutik1@gmail.com>
2026-01-27 21:02:18 -0500

    Linked List : Linked List Cycle II

--- Linked_List/detectCycle.js
+++ Linked_List/detectCycle.js
@@ -1,3 +1,5 @@
+// 142. Linked List Cycle II
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -10,13 +12,31 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// TC - O(n)
+// SC - O(1)
 var detectCycle = function (head) {
-  let slow = (fast = head);
+  let slow = head;
+  let fast = head;
 
+  // Detect cycle
   while (fast && fast.next) {
     fast = fast.next.next;
     slow = slow.next;
-    if (fast == slow) console.log(slow, fast);
+    if (fast == slow) {
+      let entry = head;
+
+      // find cycle start
+      // One pointer from head
+      // One pointer from meeting point
+      // Both walking 1 step at a time
+      // They meet exactly at cycle start
+      while (entry !== slow) {
+        entry = entry.next;
+        slow = slow.next;
+      }
+
+      return entry;
+    }
   }
 
   return null;
