3485788f309304fd3e3a48428ab0ff3a50956b89
Vrutik Parmar <parmarvrutik1@gmail.com>
2026-02-07 18:18:31 -0500

    Linked List :  Odd Even Linked List

--- Linked_List/oddEven.js
+++ Linked_List/oddEven.js
@@ -1,3 +1,5 @@
+// 328. Odd Even Linked List
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -9,20 +11,24 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// TC - O(n)
+// SC - O(1)
 var oddEvenList = function (head) {
   if (!head || !head.next) return head;
+
   let odd = head;
   let even = head.next;
-  let evenStart = even;
+  let evenHead = head.next;
 
   while (odd.next && even.next) {
     odd.next = odd.next.next;
     even.next = even.next.next;
+
     odd = odd.next;
     even = even.next;
   }
 
-  odd.next = evenStart;
+  odd.next = evenHead;
 
   return head;
 };
