6b42326c8b5c7e5ee9cdedea57303bc2271a805e
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2026-03-17 12:22:46 -0400

    Two Pointers : Valid Palindrome

--- Strings/isPalindrome.js
+++ Strings/isPalindrome.js
@@ -1,19 +1,24 @@
+// 125. Valid Palindrome
+
 /**
  * @param {string} s
  * @return {boolean}
  */
+// TC - O(n)
+// SC - O(1)
+// Using regex
 var isPalindrome = function (s) {
   s = s.toLowerCase();
   let left = 0;
   let right = s.length - 1;
 
   while (left <= right) {
-    if (!s[left].match(/[a-z0-9]/i)) {
+    if (!s[left].match(/[a-z0-9]/)) {
       left++;
       continue;
     }
 
-    if (!s[right].match(/[a-z0-9]/i)) {
+    if (!s[right].match(/[a-z0-9]/)) {
       right--;
       continue;
     }
@@ -23,6 +28,54 @@ var isPalindrome = function (s) {
     }
     left++;
     right--;
+  }
+
+  return true;
+};
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+// TC - O(n)
+// SC - O(1)
+// Using the ASCII/Unicode
+var isPalindrome = function (s) {
+  s = s.toLowerCase();
+
+  let i = 0;
+  let j = s.length - 1;
+
+  function isAlphaNumeric(ch) {
+    const code = ch.charCodeAt();
+
+    // 0-9
+    if (code >= 48 && code <= 57) return true;
+
+    // a-z
+    if (code >= 97 && code <= 122) return true;
+
+    return false;
+  }
+
+  while (i < j) {
+    // Skip left non-alphanumeric characters
+    while (i < j && !isAlphaNumeric(s[i])) {
+      i++;
+    }
+
+    // Skip right non-alphanumeric characters
+    while (i < j && !isAlphaNumeric(s[j])) {
+      j--;
+    }
+
+    // Compare valid characters
+    if (s[i] !== s[j]) {
+      return false;
+    }
+
+    i++;
+    j--;
   }
 
   return true;
