a4065ab43a9feb726e98987dce61a3659e9f2578
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2026-03-18 11:09:20 -0400

    Two Pointers/Sorting : 3Sum

--- Arrays & Math/threeSum.js
+++ Arrays & Math/threeSum.js
@@ -4,45 +4,64 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-// TC - O(n2)
-// SC - O(n)
+// This is the most optimal and desired inteview solution
+// TC - O(n^2)
+// SC - O(1) extra space ignoring output
 var threeSum = function (nums) {
   let ans = [];
+
+  // Sort the array first so that:
+  // 1. We can use two pointers
+  // 2. We can skip duplicates easily
   const arr = nums.sort((a, b) => a - b);
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr[i - 1]) {
-      twoSumII(arr, i, ans);
+  // arr.len - 2 because at least 3 numbers are needed for a triplet.
+  for (let k = 0; k < arr.length - 2; k++) {
+    // Only process this value if it is the first occurrence
+    // This avoids duplicate triplets caused by choosing the same fixed value again
+    if (arr[k] !== arr[k - 1]) {
+      twoSumII(arr, k, ans);
     }
   }
 
   return ans;
 };
 
-// 167. Two SumII - input array is sorted
-
+// Two pointer helper for 3Sum
 /**
  * @param {number[]} nums
  * @param {number} start
- * @param {number[]} ans
+ * @param {number[][]} ans
+ * @return {void}
  */
+// TC - O(n)
+// SC - O(1)
 var twoSumII = function (nums, start, ans) {
-  let i = start + 1;
-  let j = nums.length - 1;
+  // left starts just after the fixed element
+  let left = start + 1;
 
-  while (i < j) {
-    let sum = nums[i] + nums[j] + nums[start];
+  // right starts from the end of the array
+  let right = nums.length - 1;
 
-    if (sum > 0) {
-      --j;
-    } else if (sum < 0) {
-      ++i;
+  while (left < right) {
+    let sum = nums[left] + nums[right] + nums[start];
+
+    if (sum < 0) {
+      left++;
+    } else if (sum > 0) {
+      right--;
     } else {
-      ans.push([nums[i], nums[j], nums[start]]);
-      ++i;
-      --j;
-      while (i < j && nums[i] === nums[i - 1]) ++i;
-      while (i < j && nums[j] === nums[j + 1]) --j;
+      ans.push([nums[left], nums[right], nums[start]]);
+
+      // Move both pointers inward to continue searching
+      left++;
+      right--;
+
+      // Skip duplicate values on the left side
+      // Otherwise we may add the same triplet again
+      while (left < right && nums[left] === nums[left - 1]) left++;
+      // Skip duplicate values on the right side
+      while (left < right && nums[right] === nums[right + 1]) right--;
     }
   }
 };
