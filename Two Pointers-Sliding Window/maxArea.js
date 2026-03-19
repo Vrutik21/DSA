62dce3db5ef3a0076d663c7ff9fa5ed7a5594a23
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2026-03-19 09:43:35 -0400

    Sliding Window : Container With Most Water

--- Two Pointers-Sliding Window/maxArea.js
+++ Two Pointers-Sliding Window/maxArea.js
@@ -1,22 +1,20 @@
-// 11
+// 11. Container With Most Water
+
 /**
  * @param {number[]} height
  * @return {number}
  */
+// TC - O(n)
+// SC - O(1)
 var maxArea = function (height) {
-  // TC - O(n)
-  // SC - O(1)
   let i = 0;
   let j = height.length - 1;
   let maxWater = 0;
 
   while (i < j) {
-    maxWater = Math.max(
-      Math.floor(Math.min(height[i], height[j]) * (j - i)),
-      maxWater
-    );
+    maxWater = Math.max(maxWater, (j - i) * Math.min(height[i], height[j]));
 
-    if (i <= j) {
+    if (height[i] < height[j]) {
       i++;
     } else {
       j--;
@@ -25,5 +23,3 @@ var maxArea = function (height) {
 
   return maxWater;
 };
-
-console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
