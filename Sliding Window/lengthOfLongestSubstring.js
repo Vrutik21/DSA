ebd62d2d91ad61e1ab89500315f358f9643ef84f
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2026-03-20 10:22:43 -0400

    Sliding Window : Longest Substring Without Repeating Characters

--- Sliding Window/lengthOfLongestSubstring.js
+++ Sliding Window/lengthOfLongestSubstring.js
@@ -1,12 +1,16 @@
-// 3
+// 3. Longest Substring Without Repeating Characters
+
 /**
  * @param {string} s
  * @return {number}
  */
+// TC - O(n)
+// SC - O(k)
 var lengthOfLongestSubstring = function (s) {
   let longest = 0;
   let store = new Map();
-  let i = (j = 0);
+  let i = 0;
+  let j = 0;
 
   while (j < s.length) {
     const ch = s[j];
@@ -14,11 +18,10 @@ var lengthOfLongestSubstring = function (s) {
     if (store.has(ch) && store.get(ch) >= i) {
       i = store.get(ch) + 1;
     }
+
     store.set(ch, j);
-
     longest = Math.max(longest, j - i + 1);
-
-    ++j;
+    j++;
   }
 
   return longest;
