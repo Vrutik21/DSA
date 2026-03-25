f2cbf987031f518dfb2d4b526f350ad61a864fe6
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2026-03-25 11:52:39 -0400

    Binary Search : Search in Rotated Sorted Array

--- Binary Search Algorithm/search.js
+++ Binary Search Algorithm/search.js
@@ -6,34 +6,32 @@
  * @return {number}
  */
 // TC - O(log n)
-// SC - O(1)
+// SC - O(!)
 var search = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
 
   while (left <= right) {
     let mid = left + Math.floor((right - left) / 2);
-    if (nums[mid] === target) {
-      return target;
-    } else {
-      if (nums[left] <= nums[mid]) {
-        if (target < nums[mid] && target >= nums[left]) {
-          right = mid - 1;
-        } else {
-          left = mid + 1;
-        }
+
+    if (nums[mid] === target) return mid;
+
+    if (nums[left] <= nums[mid]) {
+      // Left half is sorted
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1;
       } else {
-        if (target > nums[mid] && target <= nums[right]) {
-          left = mid + 1;
-        } else {
-          right = mid - 1;
-        }
+        left = mid + 1;
+      }
+    } else {
+      // Right half is sorted
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
       }
     }
   }
 
   return -1;
 };
-
-// Input: nums = [4,5,6,7,0,1,2], target = 0
-// Output: 4
