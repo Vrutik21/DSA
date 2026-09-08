c0c9288af8db9faa9f0aa134870accbb3ae57ad2
Vrutik Parmar <78999182+Vrutik21@users.noreply.github.com>
2026-09-08 11:42:49 -0400

    Rev : Daily Temperatures

--- Revision/dailyTemperatures.ts
+++ Revision/dailyTemperatures.ts
@@ -1,22 +1,16 @@
 // 739. Daily Temperatures
 
-/**
- * @param {number[]} temperatures
- * @return {number[]}
- */
 // TC - O(n)
 // SC - O(n)
-// To improve speed, focus on recognizing this sentence faster:
-
-// "For each element, find the next greater element in the future."" - That often points to a stack-based solution
-var dailyTemperatures = function (temperatures) {
-  let n = temperatures.length;
-
-  // ans[i] stores how many days we wait for a warmer temperature
-  let ans = new Array(n).fill(0);
+// Pattern : Monotonic decreasing stack
+function dailyTemperatures(temperatures: number[]): number[] {
+  const n = temperatures.length;
 
   // stack stores indices whose next warmer day is not found yet
-  let stack = [];
+  const stack: number[] = [];
+
+  // stores how many days we wait for a warmer temperature
+  const answer = new Array(n).fill(0);
 
   for (let i = 0; i < n; i++) {
     // If today's temperature is warmer than the day at stack top,
@@ -25,15 +19,15 @@ var dailyTemperatures = function (temperatures) {
       stack.length > 0 &&
       temperatures[i] > temperatures[stack[stack.length - 1]]
     ) {
-      let prevIndex = stack.pop();
+      const prevIndex = stack.pop()!;
 
       // Difference between current day and previous day
-      ans[prevIndex] = i - prevIndex;
+      answer[prevIndex] = i - prevIndex;
     }
 
     // Current day is now waiting for a future warmer day
     stack.push(i);
   }
 
-  return ans;
-};
+  return answer;
+}
